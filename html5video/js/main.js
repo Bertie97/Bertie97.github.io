51db4f9fe12b6543ff84932d3653e02835739560
Bertie97 <2247501256@qq.com>
2018-10-18 16:26:03 +0800

    更新：10月18日

--- html5video/js/main.js
+++ html5video/js/main.js
@@ -1,10 +1,9 @@
 
 $(function() {
     var video = $('#video');
-    // $('#playpause').hide();
     $('#choices').hide();
     $('.title').hide();
-    video.click(function() {
+    playpause = function() {
         if(!video[0].paused) {
             video.trigger('pause');
             $('#playpause').html('||');
@@ -16,13 +15,15 @@ $(function() {
             $('#playpause').attr('style', 'padding-left: calc(0.3 * var(--radius));');
         }
         $('#playpause').fadeOut();
-    });
-    // video.click();
-    // var autostart = -1;
+    }
+    video.click(playpause);
+    $('#playpause').click(playpause);
+    once = false;
     setInterval(function() {
-        // if(autostart > 0) {autostart -= 1;}
-        // if(autostart == 0) {video.trigger('play');}
-        // if(autostart == -1 && video[0].readyState >= 4) {autostart = 5;}
+        if(video[0].readyState >= 3 && !once) {
+            once = true;
+            $('#video').attr('style', 'margin-top: -' + $('#video').height() / 2);
+        }
         if(video[0].currentTime > 2 && $('.title').is(':hidden')) {
             $('.title').fadeIn(2000);
         }
